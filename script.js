19307d4abe75486873f3fa265e4eedc391224b39
Your Name <you@example.com>
2025-02-25 23:47:37 +0000

    add JS box shadow to video hover

--- script.js
+++ script.js
@@ -12,17 +12,20 @@
 
 
 const sections = document.querySelectorAll(".section1, .section2, .section3");
+const container = document.querySelector(".container");
 
 sections.forEach((section) => {
   const video = section.querySelector(".background-video");
 
   section.addEventListener("mouseenter", () => {
     video.play();
+   container.style.boxShadow = "0 0 20px 20px rgba(0, 0, 0, 0.5)";
 
   });
 
   section.addEventListener("mouseleave", () => {
     video.pause();
+    container.style.boxShadow = "none";
   });
 });
 
